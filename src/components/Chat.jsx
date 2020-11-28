ecfcd0eb76b3dd0052e133b1f8f9fb130fc96363
shuntagami <shuntagami23@gmail.com>
2020-11-28 18:46:50 +0900

    プロフィール画像を表示

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import NoProfile from '../assets/img/no-profile.png'
+import Torahack from '../assets/img/torahack.png'
 
 
 const Chat = (props) => {
@@ -10,8 +12,13 @@ const Chat = (props) => {
 
   return (
     <ListItem className={classes}>
-        <ListItemAvatar>
-          <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="icon" src={Torahack} />
+        ) : (
+          <Avatar alt="icon" src={NoProfile} />
+        )}
+
         </ListItemAvatar>
         <div className="p-chat__bubble">{props.text}</div>
     </ListItem>
