0d1b9a613f747ab9663501fac39dfc06918ef5d9
shuntagami <shuntagami23@gmail.com>
2020-11-28 12:51:56 +0900

    stateの定義およびcssの設定

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import defaultDataset from "./dataset"
+import './assets/styles/style.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false
+    }
+  }
+  render() { 
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
